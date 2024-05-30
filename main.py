5790e7b940b56ff8224a12527270bc02482a18c1
Łukasz Podgórski <admin@myeye.pl>
2024-05-30 11:19:03 +0200

    Update main.py
    additional error handling

--- main.py
+++ main.py
@@ -3,23 +3,26 @@ import time
 import csv
 from datetime import datetime
 
+
 # Function to test the internet connection speed
 def test_speed():
-    st = speedtest.Speedtest()
     try:
+        st = speedtest.Speedtest()
         st.download()
         st.upload()
         results = st.results.dict()
         return results['download'] / 1000, results['upload'] / 1000  # Convert to Kb/s
-    except speedtest.SpeedtestBestServerFailure as e:
+    except (speedtest.SpeedtestBestServerFailure, speedtest.ConfigRetrievalError) as e:
         print(f"Error: {e}. Skipping this measurement.")
         return None, None
+
 
 # Function to save results to a CSV file
 def save_to_csv(timestamp, download, upload, filename='speedtest_results.csv'):
     with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([timestamp, download, upload])
+
 
 # Function for in-place countdown
 def countdown(seconds):
@@ -28,15 +31,16 @@ def countdown(seconds):
         time.sleep(1)
     print('\rStarting measurement...            ')
 
+
 # Main function of the program
 def main():
     filename = 'speedtest_results.csv'
-    
+
     # Create CSV file with headers if it doesn't exist
     with open(filename, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Timestamp', 'Download (Kb/s)', 'Upload (Kb/s)'])
-    
+
     try:
         while True:
             timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -44,9 +48,10 @@ def main():
             if download is not None and upload is not None:
                 save_to_csv(timestamp, download, upload, filename)
                 print(f'{timestamp} - Download: {download:.2f} Kb/s, Upload: {upload:.2f} Kb/s')
-            countdown(60)  # Countdown for 1 minute (60 seconds)
+            countdown(30)  # Countdown for 0,5 minutes (30 seconds)
     except KeyboardInterrupt:
         print('Program terminated.')
+
 
 if __name__ == "__main__":
     main()
